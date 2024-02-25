7835703af64ba740f70df487daaf6973b191215b
HumbalAli <160706785+HumbalAli@users.noreply.github.com>
2024-02-25 15:18:54 +0500

    Add files via upload

---
+++ practical1.js
@@ -0,0 +1,17 @@
+"use strict";
+//first practical at home 
+Object.defineProperty(exports, "__esModule", { value: true });
+//to create variable type "let" and the to what be the variable like "name" and then what should be it's value in bracket and inverted commas like ("humbal")
+let firstName = ("Humbal");
+let lastName = ("Ali");
+let age = ("14");
+let favouriteColor = ("black");
+//now to print these variable(age,name) type console.log =(variable name)
+console.log(firstName + lastName);
+console.log(age);
+console.log(favouriteColor);
+//to change the value of variable type variable name and write its value like 
+age = ("15");
+console.log(age);
+//to make boolean like true or false type
+let isStudent = true;
